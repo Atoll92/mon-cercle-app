40e2708ebda0f05ae8b7ce92833bab18ff11daee
arthur <arthur.boval@gmail.com>
2025-05-04 13:47:59 +0200

    bg nwrk ++

--- src/pages/NetworkLandingPage.jsx
+++ src/pages/NetworkLandingPage.jsx
@@ -315,7 +315,7 @@ function NetworkLandingPage() {
             left: 0,
             right: 0,
             bottom: 0,
-            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            backgroundColor: 'rgba(0, 0, 0, 0.2)',
             zIndex: 1
           }} 
         />
